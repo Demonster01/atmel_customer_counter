85907b04de8f5b4606533735cd704f00fc322710
Yurii Churov <48255311+Demonster01@users.noreply.github.com>
2019-03-14 20:03:06 +0200

    Update main.c

--- transmitter/source/main.c
+++ transmitter/source/main.c
@@ -1,1 +1,27 @@
+/*
+ * mega48_tk02_2.c
+ *
+ * Created: 14.11.2016 12:16:28
+ * Author : Demonster
+ * ychurov@i.ua
+ */ 
 
+#define F_CPU 10000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "avr035.h"
+#include "ir_transmit.h"
+
+
+int main(void)
+{
+	/* Replace with your application code */
+	ir_transmit_init();
+	sei();
+	
+	while (1)
+	{
+	}
+}
